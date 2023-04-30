ca8c7e412de565722d75feaffadaa860bcbe29f6
LucasRB7 <97030780+LucasRB7@users.noreply.github.com>
2023-04-30 12:38:45 -0300

    Formulario Os

---
+++ rsc/index.js
@@ -0,0 +1,16 @@
+// let bodyWidth = document.getElementsByTagName("body").item(0).offsetWidth;
+// let reportCliente = document.getElementsByTagName("textarea").item(0);
+// let reportOficina = document.getElementsByTagName("textarea").item(1);
+
+// let report = [document.getElementsByTagName("textarea")];
+
+// if(bodyWidth < 950){
+
+//     reportCliente.removeAttribute("cols");
+//     reportCliente.setAttribute("cols","58");
+//     reportOficina.removeAttribute("cols");
+//     reportOficina.setAttribute("cols","58");
+    
+// }
+
+// console.log(report);
