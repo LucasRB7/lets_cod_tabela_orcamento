a7ecc0129dec4f90b08660e1b1413bc23e02eab3
LucasRB7 <97030780+LucasRB7@users.noreply.github.com>
2023-05-02 11:45:09 -0300

    V9 Atualizações importantes no README.md

--- rsc/index.js
+++ rsc/index.js
@@ -28,8 +28,47 @@ const inputs = [...document.querySelectorAll("input")];
 const conteudoArea = [...document.querySelectorAll("textarea")];
 const btnApagar = document.getElementById("btn-limpar");
 
-//console.log(inputs[0].value)
+let cep = document.getElementById("cep");
+// let endereco = document.getElementById("endereco").value;
+// let bairro = document.getElementById("bairro").value;
+// let cidade = document.getElementById("cidade").value;
+// let estado = document.getElementById("estado").value;
 
+
+const buscaCep=(()=>{
+    if(cep.value != ""){        
+        let urlCep= "https://brasilapi.com.br/api/cep/v1/" + cep.value;
+        let reqApi= new XMLHttpRequest();
+        reqApi.open("GET", urlCep);
+        reqApi.send();
+        
+        reqApi.onload = () => {
+            if (reqApi.status === 200) {
+                let inforApi = JSON.parse(reqApi.response);
+
+                document.getElementById("endereco").value = inforApi.street;
+                document.getElementById("bairro").value = inforApi.neighborhood;
+                document.getElementById("cidade").value = inforApi.city;
+                document.getElementById("estado").value = inforApi.state;
+
+                // endereco = inforApi.street;
+                // bairro = inforApi.neighborhood;
+                // cidade = inforApi.city;
+                // estado = inforApi.state;
+                
+            } else if (reqApi.status === 404) {
+                alert("CEP Invalido");
+            } else {
+                alert("Erro na requisição");
+            }
+        }     
+
+    } 
+
+
+     
+
+})
 const apagar=()=>{
     inputs.map((el)=>{
         el.value=""
@@ -41,10 +80,11 @@ const apagar=()=>{
 
 }
 
-function somar(){
+const somar=(()=>{
     valorTotal.textContent = (Number(valorServico.value) + Number(valorPecas.value));
-}
-function somenteNumeros(){
+})
+
+const somenteNumeros=()=>{
     let tecla = event.keyCode;
     if(tecla >= 48 && tecla <= 57 || tecla >= 96 && tecla <= 105){
                         
@@ -55,10 +95,13 @@ function somenteNumeros(){
 }
 
 
+
 valorServico.addEventListener("keypress", somenteNumeros);
 valorPecas.addEventListener("keypress", somenteNumeros)
 btnTotal.addEventListener("click", somar);
 btnApagar.addEventListener("click", apagar );
+cep.addEventListener("blur", buscaCep);
+
 
 
 
