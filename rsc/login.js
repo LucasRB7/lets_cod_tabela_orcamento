045691b3180b7e371ee5cb3a460a976c8ceada26
LucasRB7 <97030780+LucasRB7@users.noreply.github.com>
2023-05-23 12:44:10 -0300

    v9 update

--- rsc/login.js
+++ rsc/login.js
@@ -9,6 +9,10 @@ const salvarBtn= document.getElementById("salvar-btn")
 let reg_usuario = document.getElementById("reg-usuario")
 let reg_senha1 = document.getElementById("reg-senha1")
 let reg_senha2 = document.getElementById("reg-senha2")
+let confirmacao = document.getElementById("confirmacao")
+let negacao = document.getElementById("negacao")
+let isUsuarioOk= false;
+let isSenhaOk = false;
 
 
 let listCadastrados = []
@@ -25,39 +29,50 @@ function ValidarUsuario(){
 }
 
 function ValidarCadastroUsuario(){
-    if(reg_usuario.value.length < 5){
+    if(reg_usuario.value.length == 0){
+        observacaoReg.item(0).innerHTML = ""
+        isUsuarioOk = false;  
+    }
+    else if(reg_usuario.value.length < 5){
         observacaoReg.item(0).innerHTML = "*Minimo: 5 Caracteres"
+        isUsuarioOk = false;
     }else{
         observacaoReg.item(0).innerHTML = ""        
+        isUsuarioOk = true;
     }
+    liberarBotaoSalvar()
 }
 
 function ValidarCadastroSenha(){
     if(reg_senha1.value.length < 8){
         observacaoReg.item(1).innerHTML = "*Minimo: 8 Caracteres"
+        isSenhaOk=false;
     }else if(reg_senha2.value.length < 8){
         observacaoReg.item(1).innerHTML = ""
         observacaoReg.item(2).innerHTML = "*Minimo: 8 Caracteres"
+        isSenhaOk=false;
        
-    }else{        
+    }else {        
         observacaoReg.item(2).innerHTML = " "
         ConfereSenhas()
-    }     
-    
-
+    }       
+    liberarBotaoSalvar()
 }
 function ConfereSenhas(){
     if(reg_senha1.value == reg_senha2.value){
         observacaoReg.item(1).innerHTML = "*Senhas conferem"
         observacaoReg.item(2).innerHTML = "*Senhas conferem"
+        isSenhaOk = true;
     }else{
         observacaoReg.item(1).innerHTML = "*Senhas não conferem"
         observacaoReg.item(2).innerHTML = "*Senhas não conferem"
+        isSenhaOk = false;
     }   
+    liberarBotaoSalvar()
 }
 
 function ValidarSenha(){
-    if(inpSenha.value.length < 4){
+    if(inpSenha.value.length < 8){
         observacao.item(1).innerHTML = "*Senha invalida"
               
     }else{
@@ -76,32 +91,6 @@ function login(){
     }
 }
 
-function cadastrar(){        
-    telaCadastro.style.display = "Grid";
-    
-    
-    // if(regUsuario && regUsuario.length >= 5){
-    //     let regSenha = prompt("Digite uma senha: ");
-    //     if(regSenha && regSenha.length >= 4){
-    //         let regConfirmaSenha = prompt("Confirme sua senha: ");
-    //         if(regSenha == regConfirmaSenha){
-    //             alert("Cadastro efetuado com sucesso!")
-    //             const Pessoa = {
-    //             usuario: regUsuario,
-    //             senha: regSenha,
-    //             }
-    //             listCadastrados.push(Pessoa)
-                
-    //         }else{
-    //         alert("Senha não confere, repita o procedimento!")
-    //         }            
-    //     }else{
-    //         alert("Informe uma senha válida para continuar \nMinimo de 4 caracteres")
-    //     }        
-    // }else{
-    //     alert("Informe um nome de usuario valido para continuar \nMinimo de 5 caracteres")
-    // } 
-}
 function pesquisar(){
     listCadastrados.find((e)=>{
         if(e.usuario == inpUsuario.value && e.senha == inpSenha.value){
@@ -112,13 +101,67 @@ function pesquisar(){
     })
 }
 
+function liberarBotaoSalvar(){
+    if(isUsuarioOk && isSenhaOk){
+        salvarBtn.removeAttribute("disabled")
+    }else{
+        salvarBtn.setAttribute("disabled",true)
+    }
+}
 
+function SalvarDados(){
+    const dados = listCadastrados.map((el)=>{
+           return el.usuario
+        })
+    
+    if(reg_usuario.value == dados){
+        negacao.style.display = "Flex";
+        negacao.style.color = "red"; 
+        setTimeout(()=>{
+            reg_usuario.value = ""
+            reg_senha1.value = ""
+            reg_senha2.value = ""
+            negacao.style.display = "none";             
+        },3000)
+        observacaoReg.item(1).innerHTML = ""
+        observacaoReg.item(2).innerHTML = ""
+    }else{
+        const Pessoa = {
+            usuario :  reg_usuario.value,
+            senha : reg_senha1.value,
+        }
+        listCadastrados.push(Pessoa)
+        confirmacao.style.display = "Flex";      
+        setTimeout(()=>{
+            confirmacao.style.display = "none";
+            telaCadastro.style.display = "none";  
+        },3000)
+        
+    }
+    
+}
+
+function btnClose(){
+    telaCadastro.style.display = "none";
+    reg_usuario.value = ""
+    reg_senha1.value = ""
+    reg_senha2.value = ""
+    observacaoReg.item(0).innerHTML = ""
+    observacaoReg.item(1).innerHTML = ""
+    observacaoReg.item(2).innerHTML = ""
+}
+
+function bntCadastrar(){
+    reg_usuario.value = ""
+    reg_senha1.value = ""
+    reg_senha2.value = ""
+    observacaoReg.item(0).innerHTML = ""
+    observacaoReg.item(1).innerHTML = ""
+    observacaoReg.item(2).innerHTML = ""
+    telaCadastro.style.display = "Grid";
+}
 
 btnLogin.addEventListener("click" , login)
-btnCadastro.addEventListener("click", cadastrar)
-closeBtn.addEventListener("click" , ()=>{
-    telaCadastro.style.display = "none";
-})
-salvarBtn.addEventListener("click", ()=>{
-    // if(reg_usuario.value.length > 5)
-})
+btnCadastro.addEventListener("click", bntCadastrar )
+closeBtn.addEventListener("click",btnClose)
+salvarBtn.addEventListener("click", SalvarDados)
--- rsc/page1.js
+++ rsc/page1.js
@@ -1,25 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const valorServico = document.getElementById("servicos");    
 const valorPecas = document.getElementById("pecas");
 const valorTotal = document.getElementById("total");
@@ -32,101 +10,34 @@ let [obj] = [1,2,3];
 console.log(obj);
 
 let cep = document.getElementById("cep");
-// let endereco = document.getElementById("endereco").value;
-// let bairro = document.getElementById("bairro").value;
-// let cidade = document.getElementById("cidade").value;
-// let estado = document.getElementById("estado").value;
 
 const buscarCepFetch = async ()=>{
     if(cep.value !==""){
         if(cep.value.length == 8){
                           
-                const response = fetch("https://brasilapi.com.br/api/cep/v1/"+cep.value)
-                const response2 = fetch("https://brasilapi.com.br/api/cep/v1/15503022")
-                const response3 = fetch("https://brasilapi.com.br/api/cep/v1/64006700")
-                const [resposta1, resposta2, resposta3] = await Promise.all([response, response2, response3]);
-                const [consultaCep, consultaCep2, consultaCep3] = await Promise.all([
-                    resposta1.json(), resposta2.json(), resposta3.json()
-                ])
-                if(response.status == 404){ 
-                    alert("CEP Invalido");
-                }
-                //const consultaCep = await response.json();
-                console.log({consultaCep, consultaCep2, consultaCep3})
-                document.getElementById("endereco").value = consultaCep.street;
-                document.getElementById("bairro").value = consultaCep.neighborhood;
-                document.getElementById("cidade").value = consultaCep.city;
-                document.getElementById("estado").value = consultaCep.state;
+            const response = fetch("https://brasilapi.com.br/api/cep/v1/"+cep.value)
+            const response2 = fetch("https://brasilapi.com.br/api/cep/v1/15503022")
+            const response3 = fetch("https://brasilapi.com.br/api/cep/v1/64006700")
+            const [resposta1, resposta2, resposta3] = await Promise.all([response, response2, response3]);
+            const [consultaCep, consultaCep2, consultaCep3] = await Promise.all([
+                resposta1.json(), resposta2.json(), resposta3.json()
+            ])
+            if(response.status == 404){ 
+                alert("CEP Invalido");
+            }
+            //const consultaCep = await response.json();
+            console.log({consultaCep, consultaCep2, consultaCep3})
+            document.getElementById("endereco").value = consultaCep.street;
+            document.getElementById("bairro").value = consultaCep.neighborhood;
+            document.getElementById("cidade").value = consultaCep.city;
+            document.getElementById("estado").value = consultaCep.state;
 
             
         }
         
-    }        // catch(error){
-            //     if(error.status ==404){
-}            //     alert("CEP Invalido");
-            //     }
-            //     else{
-            //     alert("Erro na requisição")
-            //     }
-            // }
-                        
-
-            // fetch("https://brasilapi.com.br/api/cep/v1/"+cep.value)
-            //     .then(response =>{           
-            //         return response.json()        
-            //     })
-            //     .then(retCep=> {            
-            //         document.getElementById("endereco").value = retCep.street;
-            //         document.getElementById("bairro").value = retCep.neighborhood;
-            //         document.getElementById("cidade").value = retCep.city;
-            //         document.getElementById("estado").value = retCep.state;            
-            //     })  
-            //     .catch(error=>{
-            //         if(error.status ==404){
-            //         alert("CEP Invalido");
-            //         }
-            //         else{
-            //         alert("Erro na requisição")
-            //         }
-            // })         
-        
-        
-    
-       
-    
-        
-
-
-// const buscaCep=(()=>{
-//     if(cep.value != ""){        
-//         let urlCep= `https://brasilapi.com.br/api/cep/v1/${cep.value}`;
-//         let reqApi= new XMLHttpRequest();
-//         reqApi.open("GET", urlCep);
-//         reqApi.send();        
-//         reqApi.onload = () => {
-//             if (reqApi.status === 200) {
-//                 let inforApi = JSON.parse(reqApi.response);
-
-//                 document.getElementById("endereco").value = inforApi.street;
-//                 document.getElementById("bairro").value = inforApi.neighborhood;
-//                 document.getElementById("cidade").value = inforApi.city;
-//                 document.getElementById("estado").value = inforApi.state;
-
-//                 // endereco = inforApi.street;
-//                 // bairro = inforApi.neighborhood;
-//                 // cidade = inforApi.city;
-//                 // estado = inforApi.state;
-                
-//             } else if (reqApi.status === 404) {
-//                 alert("CEP Invalido");
-//             } else {
-//                 alert("Erro na requisição");
-//             }
-//         }     
-
-//     }     
-
-// })
+    }        
+            
+}          
 const apagar=()=>{
     inputs.map((el)=>{
         el.value=""
@@ -152,16 +63,8 @@ const somenteNumeros=()=>{
     }
 }
 
-
-
 valorServico.addEventListener("keypress", somenteNumeros);
 valorPecas.addEventListener("keypress", somenteNumeros)
 btnTotal.addEventListener("click", somar);
 btnApagar.addEventListener("click", apagar );
 cep.addEventListener("blur", buscarCepFetch);
-
-
-
-
-//*48 a 57 || 96 a 105 numeros
-
